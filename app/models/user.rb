63fc496c27ab4b39279c5a99b69307b589749c0a
Rails Udemy Course <crescendollc@hotmail.com>
2018-11-13 03:11:42 +0000

    added user_id to articles table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, length: { minimum: 4, maximum: 50 }
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 4, maximum: 50 }, format: { with: VALID_EMAIL }
   validates :password, presence: true, length: { minimum: 4, maximum: 20 }
+  has_many :articles
 end
